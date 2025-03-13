0cca579fd02d3d06e7fd9a4066c2993018211bb4
LakshanWC <lakshanwc@gmail.com>
2025-03-13 13:50:59 +0530

    added-fronted
    added React front end part , created a basic login page

---
+++ front-end/banking-system-ui/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./Login";  // Import the Login component
+import Home from "./Home";    // Import the Home component
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
---
+++ front-end/banking-system-ui/src/Login.js
@@ -0,0 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "./UiStyel.css";
+
+function Login() {
+    const navigate = useNavigate();
+
+    const loginClicked = () => {
+        // After the login logic, navigate to the Home page
+        navigate('/Home');
+    };
+
+    return (
+        <div className={"login-container"}>
+            <h1><center>Welcome to ABC Bank</center></h1>
+            <div className={"center-middle"}>
+                <div className={"center-box"}>
+                    UserName <input type={"text"} placeholder={"user name"} /><br /><br />
+                    Password <input type={"password"} placeholder={"*******"} /><br /><br /><br />
+
+                    <button>SignUp</button>  <button onClick={loginClicked}>Login</button>
+                </div>
+            </div>
+            <div className={"page-footer"}>
+                    welcome to ABC bank
+            </div>
+        </div>
+    );
+}
+
+
+export default Login;
