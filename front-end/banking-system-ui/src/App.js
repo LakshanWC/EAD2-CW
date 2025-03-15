90bb0f879fda33a43f42493f9d7774f24fb06805
SamiduSamarasinghe <samarasinghesamidu@gmail.com>
2025-03-15 10:56:28 +0530

    gg

--- front-end/banking-system-ui/src/App.js
+++ front-end/banking-system-ui/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // src/App.js
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link, Navigate } from "react-router-dom";
@@ -5,6 +6,12 @@ import Login from "./Login"; // Import the Login component
 import Home from "./Home"; // Import the Home component
 import AccountList from "./components/AccountList"; // Import the AccountList component
 import AccountNavbar from "./components/Navbar"; // Import the Navbar component
+=======
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./Login";  // Import the Login component
+import Home from "./Home";    // Import the Home component
+import AccountList from "./components/AccountList";
+>>>>>>> Stashed changes
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false); // Track login status
@@ -18,6 +25,7 @@ function App() {
 
     return (
         <Router>
+<<<<<<< Updated upstream
             <div>
                 {/* Show microservice buttons only if logged in */}
                 {isLoggedIn && (
@@ -110,6 +118,13 @@ function App() {
                     />
                 </Routes>
             </div>
+=======
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/components" element={<AccountList />}/>
+            </Routes>
+>>>>>>> Stashed changes
         </Router>
     );
 }
